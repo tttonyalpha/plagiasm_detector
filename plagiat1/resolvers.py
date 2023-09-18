17b005a30389f34bbb2b8105acdff23036bcabcd
tttonyalpha <79598074+tttonyalpha@users.noreply.github.com>
2023-09-18 12:08:00 +0300

    Delete plagiat1 directory

--- plagiat1/resolvers.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-def shift(steps_number: int, values: List[int]) -> List[int]:
-    return [v + steps_number for v in values]
-
-  
-def mult(first: int, second: int) -> int:
-    return first * second
